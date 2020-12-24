f12be3d207306eea97d3e86acd2240456a7da959
Carla Navia <cnavia27@gmail.com>
2020-12-24 12:27:04 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -5,32 +5,43 @@ import { firebaseAppAuth, providers } from "./config/firebase";
 import { Route, Switch } from "react-router-dom";
 import Login from "./pages/LoginPage/LoginPage";
 import PrivateRoute from "./components/PrivateRoute";
+import NewTask from "./pages/NewTaskPage/NewTaskPage";
+import Navbar from "./components/Navbar/Navbar";
 
 class App extends Component {
   render() {
     const { user, signOut, signInWithGoogle } = this.props;
     return (
-      <Switch>
-        {/* <Route exact path="/signup" component={Signup} /> */}
-        <Route
-          exact
-          path="/login"
-          render={(props) => (
-            <Login
-              {...props}
-              user={user}
-              signOut={signOut}
-              signInWithGoogle={signInWithGoogle}
-            />
-          )}
-        />
-        <PrivateRoute
-          exact
-          user={user}
-          path="/"
-          component={() => <h1>TASKS PRIVATE ROUTE</h1>}
-        />
-      </Switch>
+      <div>
+        <Navbar />
+        <Switch>
+          {/* <Route exact path="/signup" component={Signup} /> */}
+          <Route
+            exact
+            path="/login"
+            render={(props) => (
+              <Login
+                {...props}
+                user={user}
+                signOut={signOut}
+                signInWithGoogle={signInWithGoogle}
+              />
+            )}
+          />
+          <PrivateRoute
+            exact
+            user={user}
+            path="/"
+            component={() => <h1>TASKS PRIVATE ROUTE</h1>}
+          />
+          <PrivateRoute
+            exact
+            user={user}
+            path="/new-task"
+            component={NewTask}
+          />
+        </Switch>
+      </div>
     );
   }
 }
