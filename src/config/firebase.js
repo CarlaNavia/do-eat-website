e0da273ce2d39ebd7d453ce525b261794fbf8ff6
Carla Navia <cnavia27@gmail.com>
2020-12-24 13:15:47 +0100

    firebase/firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,23 +1,22 @@
-import firebase from "firebase/app";
+import firebaseApp from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_AGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-const firebaseAppAuth = firebase.auth();
-const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider(),
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_AGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+if (!firebaseApp.apps.length) {
+  firebaseApp.initializeApp(firebaseConfig);
+}
 
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebaseApp.auth.GoogleAuthProvider(),
+};
 
-export { firebaseAppAuth, providers };
+export { firebaseAppAuth, providers, firebaseApp };
