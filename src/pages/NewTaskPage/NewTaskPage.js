c03f90965e38ff27e2adb1a77659b4c3410d0a67
Carla Navia <cnavia27@gmail.com>
2020-12-24 17:08:45 +0100

    new task page css done

--- src/pages/NewTaskPage/NewTaskPage.js
+++ src/pages/NewTaskPage/NewTaskPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import "./NewTaskPage.css";
+import "./NewTaskPage.css";
 import TaskService from "../../lib/task-service";
 
 class NewTask extends Component {
